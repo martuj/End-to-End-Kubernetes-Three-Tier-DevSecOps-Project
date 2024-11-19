86d060bab51aed2fbe14b880face474c9adbaf50
varikutinaresh <106514544+varikutinaresh@users.noreply.github.com>
2024-11-19 14:52:08 +0530

    Update s3.tf

--- JenkinsServer-Prerequiste/s3.tf
+++ JenkinsServer-Prerequiste/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "mehar-devsecops-bucket"
+  bucket = "mehar-devsecops-bucket1"
 
   tags = {
-    Name    = "mehar-devsecops-bucket"
+    Name    = "mehar-devsecops-bucket1"
     Project = "DevSecOps Project"
   }
 }
